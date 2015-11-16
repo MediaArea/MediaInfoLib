ba4f792f5f473c7669c2fa0c7888e31279e1fc91
Jérôme Martinez <jerome@mediaarea.net>
2015-11-16 22:54:38 +0100

    Handle of words in fields with units, not adding the unit.

--- Source/MediaInfo/MediaInfo_Config.cpp
+++ Source/MediaInfo/MediaInfo_Config.cpp
@@ -1508,8 +1508,8 @@ Ztring MediaInfo_Config::Language_Get (const Ztring &Value)
 Ztring MediaInfo_Config::Language_Get (const Ztring &Count, const Ztring &Value, bool ValueIsAlwaysSame)
 {
     //Integrity
-    if (Count.empty())
-        return EmptyString_Get();
+    if (Count.empty() || Count.find_first_not_of(__T("0123456789."))!=string::npos)
+        return Count;
 
     //Different Plurals are available or not?
     if (Language_Get(Value+__T("1")).empty())
