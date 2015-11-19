d6a44b664a798d44281d7a4f33ce510a82cd74fd
Jérôme Martinez <jerome@mediaarea.net>
2015-11-19 15:52:16 +0100

    + More known frame rates

--- Source/MediaInfo/File__Analyze_Streams.cpp
+++ Source/MediaInfo/File__Analyze_Streams.cpp
@@ -1383,6 +1383,9 @@ void File__Analyze::Video_FrameRate_Rounding(size_t Pos, video Parameter)
     float64 FrameRate_Sav=FrameRate;
 
          if (FrameRate> 9.990 && FrameRate<=10.010) FrameRate=10.000;
+    else if (FrameRate>11.984 && FrameRate<=11.094) FrameRate=11.988;
+    else if (FrameRate>11.994 && FrameRate<=12.010) FrameRate=12.000;
+    else if (FrameRate>14.980 && FrameRate<=14.990) FrameRate=14.985;
     else if (FrameRate>14.990 && FrameRate<=15.010) FrameRate=15.000;
     else if (FrameRate>23.952 && FrameRate<=23.988) FrameRate=23.976;
     else if (FrameRate>23.988 && FrameRate<=24.024) FrameRate=24.000;
