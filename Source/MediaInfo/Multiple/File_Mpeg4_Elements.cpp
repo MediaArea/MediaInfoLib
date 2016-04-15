c9fe1000ccd57fcddb9fb875dd21fdd92817dd42
Jérôme Martinez <jerome@mediaarea.net>
2016-04-15 22:04:34 +0200

    + Detection of Omneon VBI and move of the VBI track from Video part to Others part

--- Source/MediaInfo/Multiple/File_Mpeg4_Elements.cpp
+++ Source/MediaInfo/Multiple/File_Mpeg4_Elements.cpp
@@ -4113,6 +4113,15 @@ void File_Mpeg4::moov_trak_mdia_minf_stbl_stsd_xxxx()
         Fill(Stream_Video, StreamPos_Last, "WrongSubType", "Yes");
     }
 
+    //Test of VBI
+    if (StreamKind_Last == Stream_Video && Element_Code == 0x4F766269) //"Ovbi"
+    {
+        Stream_Erase(Stream_Video, StreamPos_Last);
+        Stream_Prepare(Stream_Other);
+        Streams[moov_trak_tkhd_TrackID].StreamKind = StreamKind_Last;
+        Streams[moov_trak_tkhd_TrackID].StreamPos = StreamPos_Last;
+    }
+
     FILLING_BEGIN()
         if (StreamKind_Last==Stream_Max)
         {
