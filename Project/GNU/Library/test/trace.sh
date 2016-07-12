4ad4d6aea51d992ba508ebed3bd51c49e8912671
Maxime Gervais <gervais.maxime@gmail.com>
2016-07-12 17:30:08 +0200

    Add s3 tests
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- Project/GNU/Library/test/trace.sh
+++ Project/GNU/Library/test/trace.sh
@@ -8,12 +8,12 @@ unset args
 while IFS= read -r i; do
     FILE_NAME=$(basename "$i")
     OUTPUT_XML_NAME="/tmp/$FILE_NAME.xml"
-    `$PATH_SCRIPT/mil_analyze -f XML "$PATH_FILES/$i" "$OUTPUT_XML_NAME"`
+    `$PATH_SCRIPT/mil_analyze -d -h -f XML "$PATH_FILES/$i" "$OUTPUT_XML_NAME"`
     cmd_is_ok
     xml_is_correct "$OUTPUT_XML_NAME"
     output_xml_is_a_valid_mt "$OUTPUT_XML_NAME"
     OUTPUT_MICRO_XML_NAME="/tmp/$FILE_NAME.micro.xml"
-    `$PATH_SCRIPT/mil_analyze -f MICRO_XML "$PATH_FILES/$i" "$OUTPUT_MICRO_XML_NAME"`
+    `$PATH_SCRIPT/mil_analyze -d -h -f MICRO_XML "$PATH_FILES/$i" "$OUTPUT_MICRO_XML_NAME"`
     cmd_is_ok
     xml_is_correct "$OUTPUT_MICRO_XML_NAME"
     output_xml_is_a_valid_mmt "$OUTPUT_MICRO_XML_NAME"
