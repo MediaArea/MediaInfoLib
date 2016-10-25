fd82075aacbee84e71b0311f610a486797e85cc3
pavel.pimenov <pavel.pimenov@gmail.com>
2016-10-25 20:17:34 +0300

    https://sourceforge.net/p/mediainfo/feature-requests/485/

--- Source/MediaInfo/Image/File_Bmp.cpp
+++ Source/MediaInfo/Image/File_Bmp.cpp
@@ -220,7 +220,7 @@ void File_Bmp::BitmapInfoHeader(int8u Version)
             BitsPerPixel=8; //It is a palette
 
         Fill(Stream_Image, 0, Image_Width, Width);
-        Fill(Stream_Image, 0, Image_Height, Height);
+        Fill(Stream_Image, 0, Image_Height, std::abs(int32s(Height)));
         Fill(Stream_Image, 0, Image_BitDepth, BitsPerPixel);
         Fill(Stream_Image, 0, Image_Format, Bmp_CompressionMethod(CompressionMethod));
         Fill(Stream_Image, 0, Image_Codec, Bmp_CompressionMethod(CompressionMethod));
