6c151b2d13931d2d735f2b68d9bdd6641af2b1c0
pavel.pimenov <pavel.pimenov@gmail.com>
2016-11-02 11:44:07 +0300

    Fix build with MEDIAINFO_SMPTEST0337_NO

--- Source/MediaInfo/Audio/File_SmpteSt0302.cpp
+++ Source/MediaInfo/Audio/File_SmpteSt0302.cpp
@@ -73,6 +73,7 @@ File_SmpteSt0302::~File_SmpteSt0302()
 //---------------------------------------------------------------------------
 void File_SmpteSt0302::Streams_Accept()
 {
+#ifdef MEDIAINFO_SMPTEST0337_YES
     // SMPTE ST 337
     {
         File_SmpteSt0337* SmpteSt0337=new File_SmpteSt0337();
@@ -92,7 +93,7 @@ void File_SmpteSt0302::Streams_Accept()
         #endif //MEDIAINFO_EVENTS
         Parsers.push_back(SmpteSt0337);
     }
-
+#endif // MEDIAINFO_SMPTEST0337_YES
 #ifdef MEDIAINFO_PCM_YES
     // Raw PCM
     {
