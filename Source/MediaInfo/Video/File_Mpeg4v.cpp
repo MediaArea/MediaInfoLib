44b99a7c15b25fa7851dfefed79d48ba723f6a7f
Jérôme Martinez <jerome@mediaarea.net>
2017-01-06 10:17:55 +0100

    x MPEG-4 Visual Studio profile: wrong line data_partitioned was displayed

--- Source/MediaInfo/Video/File_Mpeg4v.cpp
+++ Source/MediaInfo/Video/File_Mpeg4v.cpp
@@ -558,7 +558,7 @@ void File_Mpeg4v::Streams_Fill()
         if (user_data_start_SNC_Data[Pos][0]==__T("AlmEvent") && user_data_start_SNC_Data[Pos][1].size()==16)
             Fill(Stream_Video, 0, "Alarm event", user_data_start_SNC_Data[Pos][1]);
     }
-    if (video_object_layer_start_IsParsed && shape!=2 && !complexity_estimation_disable)
+    if (!(profile_and_level_indication >= B8(11100001) && profile_and_level_indication <= B8(11101000)) && video_object_layer_start_IsParsed && shape!=2 && !complexity_estimation_disable)
     {
         Fill(Stream_Video, 0, "data_partitioned", data_partitioned?"Yes":"No");
         Fill_SetOptions(Stream_Video, 0 ,"data_partitioned", "N NT");
