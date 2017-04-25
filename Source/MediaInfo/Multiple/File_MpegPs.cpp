74225443aad21e81f86f59fe226a0be93fd41698
Jérôme Martinez <jerome@mediaarea.net>
2017-04-25 15:08:36 +0200

    x MPEG-TS/PS: span duration across streams with same ID, avoid to span when it is not sure to be coherent (e.g. captions)

--- Source/MediaInfo/Multiple/File_MpegPs.cpp
+++ Source/MediaInfo/Multiple/File_MpegPs.cpp
@@ -702,6 +702,7 @@ void File_MpegPs::Streams_Finish_PerStream(size_t StreamID, ps_stream &Temp, kin
     }
 
     //Duration if it is missing from the parser
+    int64u Duration=0;
     if (Temp.StreamKind!=Stream_Max && Retrieve(Temp.StreamKind, Temp.StreamPos, Fill_Parameter(Temp.StreamKind, Generic_Duration)).empty())
     {
          StreamKind_Last=Temp.StreamKind;
@@ -727,7 +728,7 @@ void File_MpegPs::Streams_Finish_PerStream(size_t StreamID, ps_stream &Temp, kin
                 End+=0x200000000LL; //33 bits, cyclic
             if (Start<End)
             {
-                int64u Duration=End-Start;
+                Duration=End-Start;
                 if (ByteDifference!=(int64u)-1)
                 {
                     float BitRate=(ByteDifference*8)/(((float)Duration)/9000);
@@ -742,15 +743,21 @@ void File_MpegPs::Streams_Finish_PerStream(size_t StreamID, ps_stream &Temp, kin
                         if (FrameRate!=0)
                             Duration+=Ztring::ToZtring(90*1000/FrameRate, 0).To_int64u(); //We imagine that there is one frame in it
                     }
+                    Duration/=90;
 
-                    //Fill(StreamKind_Last, StreamPos_Last, Fill_Parameter(StreamKind_Last, Generic_Duration), Duration/90, 10, true); //TODO: refactor in order to have a more optimal way to manage several streams
-                    for (size_t StreamKind=Stream_General+1; StreamKind<Stream_Max; StreamKind++)
-                        for (size_t StreamPos=0; StreamPos<Count_Get((stream_t)StreamKind); StreamPos++)
-                            if (Retrieve((stream_t)StreamKind, StreamPos, Fill_Parameter((stream_t)StreamKind, Generic_Duration)).empty())
-                                Fill((stream_t)StreamKind, StreamPos, Fill_Parameter((stream_t)StreamKind, Generic_Duration), Duration/90, 10);
+                    Fill(Temp.StreamKind, Temp.StreamPos, Fill_Parameter(Temp.StreamKind, Generic_Duration), Duration);
                 }
             }
         }
+    }
+    if (!Duration && !Retrieve(Temp.StreamKind, Temp.StreamPos, Fill_Parameter(Temp.StreamKind, Generic_Duration)).empty())
+        Duration=Retrieve(Temp.StreamKind, Temp.StreamPos, Fill_Parameter(Temp.StreamKind, Generic_Duration)).To_int64u();
+    if (Duration)
+    {
+        for (size_t StreamKind=Stream_General+1; StreamKind<Stream_Max; StreamKind++)
+            for (size_t StreamPos=0; StreamPos<Count_Get((stream_t)StreamKind); StreamPos++)
+                if ((IsSub || (KindOfStream==KindOfStream_Main && Retrieve((stream_t)StreamKind, StreamPos, General_ID).To_int64u()==StreamID)) && Retrieve((stream_t)StreamKind, StreamPos, Fill_Parameter((stream_t)StreamKind, Generic_Duration)).empty())
+                    Fill((stream_t)StreamKind, StreamPos, Fill_Parameter((stream_t)StreamKind, Generic_Duration), Duration);
     }
 
     //Bitrate calculation
