c944a8dd3296348b4b77c2de5645a00367aa7a68
Jérôme Martinez <jerome@mediaarea.net>
2017-10-22 20:30:29 +0200

    x Fix crash

--- Source/MediaInfo/MediaInfo_Internal.cpp
+++ Source/MediaInfo/MediaInfo_Internal.cpp
@@ -809,6 +809,8 @@ size_t MediaInfo_Internal::Open_Buffer_SegmentChange ()
 {
     MEDIAINFO_DEBUG_CONFIG_TEXT(Debug+=__T("Open_Buffer_SegmentChange"))
 
+    if (Info == NULL)
+        return 0;
     Info->Open_Buffer_SegmentChange();
 
     return 1;
