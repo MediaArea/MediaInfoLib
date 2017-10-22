c9386acff767595ce3a29a4e78515815fb8ea97e
Jérôme Martinez <jerome@mediaarea.net>
2017-10-22 20:42:01 +0200

    Additional check with MOV referencing files

--- Source/MediaInfo/Reader/Reader_File.cpp
+++ Source/MediaInfo/Reader/Reader_File.cpp
@@ -663,6 +663,7 @@ size_t Reader_File::Format_Test_PerParser_Continue (MediaInfo_Internal* MI)
                 #if MEDIAINFO_READTHREAD
                     && (!ThreadInstance || (!IsLooping && Buffer_Begin+MI->Config.File_Buffer_Size>=Buffer_End))    //File buffer hit the end of buffer
                 #endif //MEDIAINFO_READTHREAD
+             && F.Opened_Get()                                                                                      //File must be still open
              && MI->Config.File_Current_Offset+F.Position_Get()>=MI->Config.File_Size                               //File read hit the end of file
              && MI->Config.File_Names.size()==1) //TODO: fix about sequences of files
             {
