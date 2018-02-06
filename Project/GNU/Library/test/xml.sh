6bb8ba12d4b62d3ebb63b5eb3922ade737ae7420
Maxime Gervais <gervais.maxime@gmail.com>
2018-02-06 11:18:44 +0100

    MediaInfo xml validity test
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

---
+++ Project/GNU/Library/test/xml.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+PATH_SCRIPT=$(dirname "$0")
+PATH_FILES=$PATH_SCRIPT/Files
+. "$PATH_SCRIPT/utils.sh"
+
+unset args
+while IFS= read -r i; do
+    FILE_NAME=$(basename "$i")
+    OUTPUT_XML_NAME="/tmp/$FILE_NAME.xml"
+    `$PATH_SCRIPT/mil_analyze -f XML "$PATH_FILES/$i" "$OUTPUT_XML_NAME"`
+    cmd_is_ok
+    xml_is_correct "$OUTPUT_XML_NAME"
+    output_xml_is_a_valid_mi "$OUTPUT_XML_NAME"
+done < "$PATH_FILES/files.txt"
