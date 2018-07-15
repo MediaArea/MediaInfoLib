d588febaf172f9300701378727e06bc985fb5c87
Jérôme Martinez <jerome@mediaarea.net>
2018-07-15 14:27:59 +0200

    x AC-3/E-AC-3 interleaved: permit ec3 extension

--- Source/MediaInfo/File__Analyze_Streams_Finish.cpp
+++ Source/MediaInfo/File__Analyze_Streams_Finish.cpp
@@ -461,8 +461,8 @@ void File__Analyze::Streams_Finish_StreamOnly_General(size_t StreamPos)
     //Exception
     if (Retrieve(Stream_General, 0, General_Format)==__T("AC-3") && Retrieve(Stream_General, 0, General_Format_Profile).find(__T("E-AC-3"))==0)
     {
-        Fill(Stream_General, 0, General_Format_Extensions, "ac3 eb3", Unlimited, true, true);
-        Fill(Stream_General, 0, General_Codec_Extensions, "ac3 eb3", Unlimited, true, true);
+        Fill(Stream_General, 0, General_Format_Extensions, "ac3 eb3 ec3", Unlimited, true, true); //ec3 added because sauf reference files use it
+        Fill(Stream_General, 0, General_Codec_Extensions, "ac3 eb3 ec3", Unlimited, true, true);
     }
 
     //File extension test
