cf42a9c90dc9e99e27c080e30d841e7e20685f12
Jérôme Martinez <jerome@mediaarea.net>
2018-08-24 09:44:42 +0200

    JNI: remove duplicate license header

--- Source/MediaInfoDLL/MediaInfoDLL.JNI.java
+++ Source/MediaInfoDLL/MediaInfoDLL.JNI.java
@@ -4,12 +4,6 @@
  *  be found in the License.html file in the root of the source tree.
  */
 
-/*  Copyright (c) MediaArea.net SARL. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license that can
- *  be found in the License.html file in the root of the source tree.
- */
-
 class MediaInfo
 {
     static
