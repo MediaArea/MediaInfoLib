8ba9d392c416fccbde78d7202be34f9d3b26287d
Jérôme Martinez <jerome@mediaarea.net>
2019-03-25 20:50:03 +0100

    + Blackmagic RAW detection

--- Source/MediaInfo/Multiple/File_Mpeg4.h
+++ Source/MediaInfo/Multiple/File_Mpeg4.h
@@ -368,6 +368,7 @@ private :
     int32u                                  moov_mvhd_TimeScale;
     int32u                                  Vendor;
     Ztring                                  Vendor_Version;
+    Ztring                                  DisplayAspectRatio;
     int64u                                  FirstMdatPos;
     int64u                                  LastMdatPos; //This is the position of the byte after the last byte of mdat
     int64u                                  FirstMoovPos;
