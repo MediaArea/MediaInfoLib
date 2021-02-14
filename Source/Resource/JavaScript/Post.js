aee74f6013964289e7c64176f1688c778c7a78b7
Maxime Gervais <gervais.maxime@gmail.com>
2021-02-15 00:22:43 +0100

    JavaScript: Support emscripten v2.x
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- Source/Resource/JavaScript/Post.js
+++ Source/Resource/JavaScript/Post.js
@@ -1,64 +1,54 @@
-return function (args) {
- var module, helpers = function () {
-    module.MediaInfo.prototype.Open = function (file, callback) {
-      var _this = this, offset = 0, CHUNK_SIZE = 1024 * 1024;
+helpers = function() {
+  MediaInfoLib.MediaInfo.prototype.Open = function(file, callback) {
+    var _this = this, offset = 0, CHUNK_SIZE = 1024 * 1024;
 
-      if (file.constructor !== File)
-        return; //wrong argument
+    if (file.constructor !== File)
+      return; //wrong argument
 
-      _this.Option('File_FileName', file.name);
-      _this.Open_Buffer_Init(file.size, 0);
+    _this.Option('File_FileName', file.name);
+    _this.Open_Buffer_Init(file.size, 0);
 
-      var loop = function(length) {
-        var r = new FileReader();
-        var blob = file.slice(offset, offset + length);
-        r.onload = processChunk;
-        r.readAsArrayBuffer(blob);
-      };
+    var loop = function(length) {
+      var r = new FileReader();
+      var blob = file.slice(offset, offset + length);
+      r.onload = processChunk;
+      r.readAsArrayBuffer(blob);
+    };
 
-      var processChunk = function(e) {
-        if (e.target.error === null) {
-          var state = _this.Open_Buffer_Continue(e.target.result);
+    var processChunk = function(e) {
+      if (e.target.error === null) {
+        var state = _this.Open_Buffer_Continue(e.target.result);
 
-          //Test if there is a MediaInfo request to go elsewhere
-          var seekTo = _this.Open_Buffer_Continue_Goto_Get();
-          if(seekTo === -1) {
-            offset += e.target.result.byteLength;
-          } else {
-            offset = seekTo;
-            _this.Open_Buffer_Init(file.size, seekTo); // Inform MediaInfo we have seek
-          }
+        //Test if there is a MediaInfo request to go elsewhere
+        var seekTo = _this.Open_Buffer_Continue_Goto_Get();
+        if(seekTo === -1) {
+          offset += e.target.result.byteLength;
         } else {
-          typeof callback==="function"&&callback(); // Error
-          return;
+          offset = seekTo;
+          _this.Open_Buffer_Init(file.size, seekTo); // Inform MediaInfo we have seek
         }
+      } else {
+        typeof callback==="function"&&callback(); // Error
+        return;
+      }
 
-        // Bit 3 set means finalized
-        if (state&0x08 || e.target.result.byteLength < 1) {
-          _this.Open_Buffer_Finalize();
-          typeof callback==="function"&&callback();
-          return;
-        }
-        loop(CHUNK_SIZE);
-      };
-
-      // Start
+      // Bit 3 set means finalized
+      if (state&0x08 || e.target.result.byteLength < 1) {
+        _this.Open_Buffer_Finalize();
+        typeof callback==="function"&&callback();
+        return;
+      }
       loop(CHUNK_SIZE);
     };
-  };
 
-  if (arguments.length) {
-    if (typeof arguments[0]['postRun'] === 'undefined') {
-        arguments[0]['postRun'] = [helpers];
-    } else if (typeof arguments[0]['postRun'] === 'array') {
-        arguments[0]['postRun'].unshift(helpers);
-    } else if (typeof arguments[0]['postRun'] === 'function') {
-        arguments[0]['postRun'] = [helpers, arguments[0]['postRun']];
-    }
-  } else {
-    arguments = [ {'postRun': [helpers] } ];
-  }
-  module = Module.apply(this, arguments);
-  return module;
+    // Start
+    loop(CHUNK_SIZE);
+  };
 };
-}));
+
+if (typeof MediaInfoLib.ready !== 'undefined') {
+  MediaInfoLib.ready.then(helpers);
+}
+else {
+  addOnPostRun(helpers);
+}
