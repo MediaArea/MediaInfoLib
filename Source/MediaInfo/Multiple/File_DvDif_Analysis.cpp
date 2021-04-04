2a737b2c28321469bb26d2758d7f2e378658e7b3
Jérôme Martinez <jerome@mediaarea.net>
2021-04-05 00:25:01 +0200

    x DV: fix PTS/DUR info
    Note: PTS is related to the DV stream only, container PTS offset e.g. QuickTime edit list is not (yet) handled.

--- Source/MediaInfo/Multiple/File_DvDif_Analysis.cpp
+++ Source/MediaInfo/Multiple/File_DvDif_Analysis.cpp
@@ -678,6 +678,12 @@ void File_DvDif::Errors_Stats_Update()
             return;
     }
 
+    FrameInfo.DTS=FrameInfo.PTS=Speed_FrameCount_system[0]*100100000/3+Speed_FrameCount_system[1]*40000000;
+    Speed_FrameCount_system[system]++;
+    int64u NextPTS=Speed_FrameCount_system[0]*100100000/3+Speed_FrameCount_system[1]*40000000;
+    Speed_FrameCount_system[system]--;
+    FrameInfo.DUR=NextPTS-FrameInfo.PTS; // PTS + DUR = PTS of next frame, DUR rounding is adapted in order to have the exact PTS of next frame
+
     Ztring Errors_Stats_Line;
     {
         bool Errors_AreDetected=false;
@@ -725,6 +731,7 @@ void File_DvDif::Errors_Stats_Update()
 
         EVENT_BEGIN(DvDif, Change, 0)
             Event.StreamOffset=Speed_FrameCount_StartOffset;
+            Event.FrameNumber=Speed_FrameCount;
             switch (video_source_stype)
             {
                 case 0x00 :
@@ -1652,6 +1659,7 @@ void File_DvDif::Errors_Stats_Update()
             struct MediaInfo_Event_DvDif_Analysis_Frame_1 Event1;
             Event_Prepare((struct MediaInfo_Event_Generic*)&Event1, MediaInfo_EventCode_Create(MediaInfo_Parser_DvDif, MediaInfo_Event_DvDif_Analysis_Frame, 1), sizeof(MediaInfo_Event_DvDif_Analysis_Frame_1));
             Event1.StreamOffset=Speed_FrameCount_StartOffset;
+            Event1.FrameNumber=Speed_FrameCount;
             Event1.TimeCode=Event.TimeCode;
             Event1.RecordedDateTime1=Event.RecordedDateTime1;
             Event1.RecordedDateTime2Buggy=Event.RecordedDateTime2;
@@ -1792,6 +1800,7 @@ void File_DvDif::Errors_Stats_Update()
     Speed_Arb_Last=Speed_Arb_Current;
     Speed_Arb_Current.Clear();
     Speed_FrameCount++;
+    Speed_FrameCount_system[system]++;
     REC_IsValid=false;
     audio_source_mode.clear();
     Speed_Contains_NULL=0;
