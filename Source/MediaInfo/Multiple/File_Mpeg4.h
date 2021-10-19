56bc23f26491a8facca9a7dc1c055df143fbab72
Zhao Zhili <zhilizhao@tencent.com>
2021-10-19 10:50:11 +0800

    Fix mp4 edts trace info
    1. Media time is signed value
    2. Media time is in unit of mdhd time scale. Since mdhd comes after
    edts, we don't know mdhd_TimeScale yet when handle edts. Give a hint
    to user on which time scale to use.
    
    Fix #1441

--- Source/MediaInfo/Multiple/File_Mpeg4.h
+++ Source/MediaInfo/Multiple/File_Mpeg4.h
@@ -434,7 +434,7 @@ private :
         struct edts_struct
         {
             int64u  Duration;
-            int64u  Delay;
+            int64s  Delay;
             int32u  Rate;
         };
         std::vector<edts_struct> edts;
