469cfe022388c5c69a28e41c0cf5f70a8e9b8a5a
cjee21 <77721854+cjee21@users.noreply.github.com>
2025-10-07 16:07:14 +0800

    Rust wrapper and example

---
+++ Source/Example/HowToUse_Dll-rs/src/main.rs
@@ -0,0 +1,161 @@
+/*  Copyright (c) MediaArea.net SARL. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license that can
+ *  be found in the License.html file in the root of the source tree.
+ */
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//
+// Example for MediaInfoLib
+// Command line version
+//
+// Execute 'cargo run --release' to build and run with default 'Example.ogg'
+// or 'cargo run --release -- <path to a file>' to use other file
+//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+use mediainfo_rs::{MediaInfo, MediaInfo_info_t::*, MediaInfo_stream_t::*};
+use std::{
+    env,
+    fs::File,
+    io::{self, Read, Seek, SeekFrom},
+};
+
+fn main() {
+    // Get optional command-line argument for filename
+    let args: Vec<String> = env::args().collect();
+    let optional_arg = if args.len() > 1 { Some(&args[1]) } else { None };
+    let file = match optional_arg {
+        Some(arg_value) => arg_value,   //Use user-provided file if any
+        None => "../../../Example.ogg", //else use Example file
+    };
+
+    // Information about MediaInfo
+    let mi = MediaInfo::new();
+    let mut to_display = mi.option("Info_Version", "0.7.13;MediaInfoDLL_Example_MSVC;0.7.13");
+
+    to_display += "\r\n\r\nInfo_Parameters\r\n";
+    to_display += &mi.option("Info_Parameters", "");
+
+    to_display += "\r\n\r\nInfo_Codecs\r\n";
+    to_display += &mi.option("Info_Codecs", "");
+
+    // An example of how to use the library
+    to_display += "\r\n\r\nOpen\r\n";
+    mi.open(file);
+
+    to_display += "\r\n\r\nInform with Complete=false\r\n";
+    mi.option("Complete", "");
+    to_display += &mi.inform();
+
+    to_display += "\r\n\r\nInform with Complete=true\r\n";
+    mi.option("Complete", "1");
+    to_display += &mi.inform();
+
+    to_display += "\r\n\r\nCustom Inform\r\n";
+    mi.option("Inform", "General;Example : FileSize=%FileSize%");
+    to_display += &mi.inform();
+
+    to_display += "\r\n\r\nGet with Stream=General and Parameter=\"FileSize\"\r\n";
+    to_display += &mi.get(
+        MediaInfo_Stream_General,
+        0,
+        "FileSize",
+        MediaInfo_Info_Text,
+        MediaInfo_Info_Name,
+    );
+
+    to_display += "\r\n\r\nGetI with Stream=General and Parameter=90\r\n";
+    to_display += &mi.get_i(MediaInfo_Stream_General, 0, 90, MediaInfo_Info_Text);
+
+    to_display += "\r\n\r\nCount_Get with StreamKind=Stream_Audio\r\n";
+    to_display += &mi.count_get(MediaInfo_Stream_Audio, usize::MAX).to_string();
+
+    to_display += "\r\n\r\nGet with Stream=General and Parameter=\"AudioCount\"\r\n";
+    to_display += &mi.get(
+        MediaInfo_Stream_General,
+        0,
+        "AudioCount",
+        MediaInfo_Info_Text,
+        MediaInfo_Info_Name,
+    );
+
+    to_display += "\r\n\r\nGet with Stream=General and Parameter=\"StreamCount\"\r\n";
+    to_display += &mi.get(
+        MediaInfo_Stream_General,
+        0,
+        "StreamCount",
+        MediaInfo_Info_Text,
+        MediaInfo_Info_Name,
+    );
+
+    to_display += "\r\n\r\nClose\r\n";
+    mi.close();
+
+    println!("{to_display}");
+
+    // By buffer example
+    by_buffer_example(file).unwrap();
+}
+
+//***************************************************************************
+// By buffer example
+//***************************************************************************
+fn by_buffer_example(file: &str) -> io::Result<()> {
+    //From: preparing an example file for reading
+    let mut f = File::open(file)?;
+
+    //From: preparing a memory buffer for reading
+    let mut from_buffer = [0; 7 * 188]; //Note: you can do your own buffer
+    let mut from_buffer_size; //The size of the read file buffer
+
+    //From: retrieving file size
+    f.seek(SeekFrom::End(0))?;
+    let f_size = f.stream_position()?;
+    f.seek(SeekFrom::Start(0))?;
+
+    //Initializing MediaInfo
+    let mi = MediaInfo::new();
+
+    //Preparing to fill MediaInfo with a buffer
+    mi.open_buffer_init(f_size, 0);
+
+    //The parsing loop
+    loop {
+        //Reading data somewhere, do what you want for this.
+        from_buffer_size = f.read(&mut from_buffer)?;
+
+        //Sending the buffer to MediaInfo
+        let status = mi.open_buffer_continue(&from_buffer, from_buffer_size);
+        if status & 0x08 == 0x08 {
+            //Bit3=Finished
+            break;
+        }
+
+        //Testing if there is a MediaInfo request to go elsewhere
+        if mi.open_buffer_continue_goto_get() != u64::MAX {
+            f.seek(SeekFrom::Start(mi.open_buffer_continue_goto_get()))?; //Position the file
+            mi.open_buffer_init(f_size, f.stream_position()?); //Informing MediaInfo we have seek
+        }
+
+        if from_buffer_size == 0 {
+            break;
+        }
+    }
+
+    //Finalizing
+    mi.open_buffer_finalize(); //This is the end of the stream, MediaInfo must finnish some work
+
+    //Get() example
+    let to_display = mi.get(
+        MediaInfo_Stream_General,
+        0,
+        "Format",
+        MediaInfo_Info_Text,
+        MediaInfo_Info_Name,
+    );
+
+    println!("{to_display}");
+
+    Ok(())
+}
