MediaArea/MediaInfoLib

commit 6e03ccb27a293dc51c80595885bc6001f0f1deb9

+ I1881, MXF & MOV: customizable seek pos and duration of caption probe

Source/MediaInfo/Multiple/File_Mxf.cpp
@@ -19186,14 +19186,86 @@ bool File_Mxf::BookMark_Needed()
 {
     Frame_Count_NotParsedIncluded=(int64u)-1;
 
-    if (MayHaveCaptionsInStream && !IsSub && IsParsingEnd && File_Size!=(int64u)-1 && Config->ParseSpeed>0 && Config->ParseSpeed<1 && IsParsingMiddle_MaxOffset==(int64u)-1 && File_Size/2>0x4000000) //TODO: 64 MB by default; // Do not search in the middle of the file if quick pass or full pass
+    if (MayHaveCaptionsInStream && !IsSub && IsParsingEnd && IsParsingMiddle_MaxOffset==(int64u)-1)
     {
-        IsParsingMiddle_MaxOffset=File_Size/2+0x4000000; //TODO: 64 MB by default;
-        GoTo(File_Size/2);
-        Open_Buffer_Unsynch();
-        IsParsingEnd=false;
-        IsCheckingRandomAccessTable=false;
-        Streams_Count=(size_t)-1;
+        int64u ProbeCaptionBytePos=(int64u)-1;
+        int64u ProbeCaptionByteDur=(int64u)-1;
+        int64u Duration=0;
+        for (size_t StreamKind=Stream_General; StreamKind<Stream_Max; StreamKind++)
+        {
+            auto Count=Count_Get((stream_t)StreamKind);
+            for (size_t StreamPos=0; StreamPos<Count; StreamPos++)
+            {
+                Duration=Retrieve_Const((stream_t)StreamKind, StreamPos, Fill_Parameter((stream_t)StreamKind, Generic_Duration)).To_int64u()/1000;
+                if (Duration)
+                    break;
+            }
+            if (Duration)
+                break;
+        }
+        if (!Duration)
+        {
+            for (const auto& Track : Tracks)
+            {
+                if (!Track.second.EditRate)
+                    continue;
+                const auto& Component=Components.find(Track.second.Sequence);
+                if (Component!=Components.end())
+                {
+                    if (Component->second.Duration && Component->second.Duration!=(int64u)-1)
+                        Duration=Component->second.Duration/Track.second.EditRate;
+                }
+            }
+        }
+        auto Probe=Config->File_ProbeCaption_Get(ParserName);
+        switch (Probe.Start_Type)
+        {
+            case config_probe_size:
+                ProbeCaptionBytePos=Probe.Start;
+                break;
+            case config_probe_dur:
+                if (Duration)
+                {
+                    Probe.Start=Probe.Start*100/Duration; //TODO: real timestamp
+                    if (!Probe.Start)
+                        Probe.Start=1;
+                }
+                else
+                    Probe.Start=50;
+                // Fall through
+            case config_probe_percent:
+                ProbeCaptionBytePos=File_Size/100*Probe.Start;
+                break;
+        }
+        switch (Probe.Duration_Type) {
+            case config_probe_size:
+                ProbeCaptionByteDur=Probe.Duration;
+                break;
+            case config_probe_dur:
+                if (Duration)
+                {
+                    Probe.Duration=Probe.Duration*100/Duration; //TODO: real timestamp
+                    if (!Probe.Duration)
+                        Probe.Duration++;
+                }
+                else
+                    Probe.Duration=1;
+                // Fall through
+            case config_probe_percent:
+                ProbeCaptionByteDur=File_Size/100*Probe.Duration;
+                break;
+        }
+        auto MaxOffset=ProbeCaptionBytePos+ProbeCaptionByteDur;
+        auto CurrentEnd=File_Offset+Buffer_Offset;
+        if (ProbeCaptionBytePos!=(int64u)-1 && ProbeCaptionByteDur!=(int64u)-1 && File_Size/2>ProbeCaptionByteDur)
+        {
+            IsParsingMiddle_MaxOffset=MaxOffset;
+            GoTo(ProbeCaptionBytePos);
+            Open_Buffer_Unsynch();
+            IsParsingEnd=false;
+            IsCheckingRandomAccessTable=false;
+            Streams_Count=(size_t)-1;
+        }
     }
 
     if (ExtraMetadata_Offset!=(int64u)-1)

Source/MediaInfo/MediaInfo_Config_MediaInfo.cpp
@@ -18,6 +18,7 @@
 //---------------------------------------------------------------------------
 #include "MediaInfo/MediaInfo_Config_MediaInfo.h"
 #include "MediaInfo/MediaInfo_Config.h"
+#include "MediaInfo/TimeCode.h"
 #include "ZenLib/ZtringListListF.h"
 #if MEDIAINFO_EVENTS
     #include "ZenLib/FileName.h"
@@ -244,6 +245,7 @@ MediaInfo_Config_MediaInfo::MediaInfo_Config_MediaInfo()
     File_Eia608_DisplayEmptyStream=false;
     File_Eia708_DisplayEmptyStream=false;
     File_CommandOnlyMeansEmpty=false;
+    File_ProbeCaption_Set({});
     State=0;
     #if defined(MEDIAINFO_AC3_YES)
     File_Ac3_IgnoreCrc=false;
@@ -1263,6 +1265,15 @@ Ztring MediaInfo_Config_MediaInfo::Option (const String &Option, const String &V
     else if (Option_Lower==__T("file_commandonlymeansempty_get"))
     {
         return File_CommandOnlyMeansEmpty_Get()?"1":"0";
+    }
+    else if (Option_Lower==__T("file_probecaption"))
+    {
+        File_ProbeCaption_Set(Value);
+        return __T("");
+    }
+    else if (Option_Lower==__T("file_probecaption_get"))
+    {
+        return __T("(Not supported)");
     }
     else if (Option_Lower==__T("file_ac3_ignorecrc"))
     {
@@ -3594,6 +3605,135 @@ bool MediaInfo_Config_MediaInfo::File_CommandOnlyMeansEmpty_Get ()
 {
     CriticalSectionLocker CSL(CS);
     return File_CommandOnlyMeansEmpty;
+}
+
+//---------------------------------------------------------------------------
+Ztring MediaInfo_Config_MediaInfo::File_ProbeCaption_Set (const Ztring& NewValue)
+{
+    static const auto Malformed = __T("File_ProbeCaption option malformed");
+    ZtringListList List;
+    List.Separator_Set(0, __T(","));
+    List.Separator_Set(1, __T("+"));
+    List.Write(Ztring(NewValue).MakeUpperCase());
+    CriticalSectionLocker CSL(CS);
+    File_ProbeCaption.clear();
+    bool HasForAll = false;
+    string AllMinus;
+    for (const auto& Line : List) {
+        config_probe Item;
+        for (const auto& Value : Line) {
+            auto Pos = Value.find_first_not_of(__T("0123456789"));
+            if (Pos == string::npos) {
+                Item.Start_Type = config_probe_size;
+                Item.Start = Value.To_int64u();
+            }
+            else if (!Value.empty() && Value[0] >= __T('a') && Value[0] <= __T('z')) {
+                string Value2;
+                bool Negative = false;
+                if (Value.rfind(__T("all"), 0) == 0) {
+                    if (Value.size() > 3) {
+                        if (Value[3] != __T('-'))
+                            return Malformed;
+                        Negative = true;
+                        Value2 = Value.To_UTF8().substr(4);
+                        AllMinus += '-';
+                        AllMinus += Value2;
+                    }
+                }
+                else {
+                    Value2 = Value.To_UTF8();
+                }
+                if (Value2 != "MP4" && Value2 != "MPEG-4" && Value2 != "MXF" && !Value2.empty()) { //TODO: full list
+                    return Malformed;
+                }
+                Item.Parser = Value2 != "MP4" ? Value2 : "MPEG-4";
+                if (Negative) {
+                    Item.Parser.insert(Item.Parser.begin(), __T('-'));
+                }
+            }
+            else if (Pos == Value.size() - 1 && Value[Pos] == __T('%')) {
+                auto Value_Int = Value.To_int64u();
+                if (Value_Int > 100) {
+                    return Malformed;
+                }
+                if (Item.Start_Type == config_probe_none) {
+                    Item.Start_Type = config_probe_percent;
+                    Item.Start = Value_Int;
+                }
+                else if (Item.Duration_Type == config_probe_none) {
+                    Item.Duration_Type = config_probe_percent;
+                    Item.Duration = Value_Int;
+                }
+                else {
+                    return Malformed;
+                }
+            }
+            else {
+                TimeCode TC(Value.To_UTF8());
+                if (!TC.IsValid()) {
+                    return Malformed;
+                }
+                auto Seconds = TC.ToSeconds();
+                if (Seconds <= 0) {
+                    return Malformed;
+                }
+                if (Item.Start_Type == config_probe_none) {
+                    Item.Start_Type = config_probe_dur;
+                    Item.Start = Seconds;
+                }
+                else if (Item.Duration_Type == config_probe_none) {
+                    Item.Duration_Type = config_probe_dur;
+                    Item.Duration = Seconds;
+                }
+                else {
+                    return Malformed;
+                }
+            }
+        }
+        if (!Item.Parser.empty()) {
+            HasForAll=true;
+        }
+        File_ProbeCaption.push_back(Item);
+    }
+    File_ProbeCaption_Pos=0;
+    if (!HasForAll) {
+        config_probe Probe;
+        Probe.Start_Type = config_probe_percent;
+        Probe.Start = 50;
+        Probe.Duration_Type = config_probe_dur;
+        Probe.Duration = 30;
+        Probe.Parser = AllMinus;
+        File_ProbeCaption.push_back(Probe);
+    }
+
+    return {};
+}
+
+config_probe MediaInfo_Config_MediaInfo::File_ProbeCaption_Get(const string& ParserName)
+{
+    if (ParseSpeed<=0 && ParseSpeed>=1)
+        return {};
+
+    CriticalSectionLocker CSL(CS);
+    for (;;)
+    {
+        if (File_ProbeCaption_Pos >= File_ProbeCaption.size())
+            return {};
+        const auto& Item = File_ProbeCaption[File_ProbeCaption_Pos];
+        File_ProbeCaption_Pos++;
+        if (Item.Parser.empty()) {
+            if (Item.Parser[0] == '-') {
+                if (Item.Parser.rfind(ParserName, 1) == 1) {
+                    continue;
+                }
+            }
+        }
+        else if (Item.Parser != ParserName) {
+            continue;
+        }
+
+        return Item;
+    }
 }
 
 //---------------------------------------------------------------------------

Source/MediaInfo/MediaInfo_Config_MediaInfo.h
@@ -68,6 +68,22 @@ enum encryption_padding
 };
 #endif //MEDIAINFO_AES
 
+enum config_probe_type
+{
+    config_probe_none,
+    config_probe_size,
+    config_probe_dur,
+    config_probe_percent,
+};
+struct config_probe
+{
+    config_probe_type   Start_Type=config_probe_none;
+    config_probe_type   Duration_Type=config_probe_none;
+    int64u              Start=0;
+    int64u              Duration=0;
+    string              Parser;
+};
+
 //***************************************************************************
 // Class MediaInfo_Config_MediaInfo
 //***************************************************************************
@@ -377,6 +393,8 @@ public :
     bool          File_Eia708_DisplayEmptyStream_Get ();
     void          File_CommandOnlyMeansEmpty_Set(bool NewValue);
     bool          File_CommandOnlyMeansEmpty_Get();
+    Ztring        File_ProbeCaption_Set(const Ztring& NewValue);
+    config_probe  File_ProbeCaption_Get(const string& Parser);
     #if defined(MEDIAINFO_AC3_YES)
     void          File_Ac3_IgnoreCrc_Set (bool NewValue);
     bool          File_Ac3_IgnoreCrc_Get ();
@@ -612,6 +630,8 @@ private :
     bool                    File_Eia608_DisplayEmptyStream;
     bool                    File_Eia708_DisplayEmptyStream;
     bool                    File_CommandOnlyMeansEmpty;
+    std::vector<config_probe> File_ProbeCaption;
+    size_t                  File_ProbeCaption_Pos;
     #if defined(MEDIAINFO_AC3_YES)
     bool                    File_Ac3_IgnoreCrc;
     #endif //defined(MEDIAINFO_AC3_YES)

Source/MediaInfo/Multiple/File_Mpeg4.cpp
@@ -2589,6 +2589,8 @@ bool File_Mpeg4::BookMark_Needed()
                                     else
                                         mdat_Pos_Temp2.Size = *stsz_Current;
                                     mdat_Pos.push_back(mdat_Pos_Temp2);
+                                    if (Temp->second.IsCaption)
+                                        mdat_Pos_Caption.push_back(mdat_Pos_Temp2);
                                     Chunk_Offset += *stsz_Current;
                                     stsz_Current++;
                                     if (stsz_Current2)
@@ -2607,6 +2609,8 @@ bool File_Mpeg4::BookMark_Needed()
                             mdat_Pos_Temp2.StreamID = Temp->first;
                             mdat_Pos_Temp2.Size = stsc_Current->SamplesPerChunk*Temp->second.stsz_Sample_Size*Temp->second.stsz_Sample_Multiplier;
                             mdat_Pos.push_back(mdat_Pos_Temp2);
+                            if (Temp->second.IsCaption)
+                                mdat_Pos_Caption.push_back(mdat_Pos_Temp2);
 
                             #if MEDIAINFO_DEMUX
                                 if (Temp_stts_Durations.empty() || stsc_Current->SamplesPerChunk != Temp_stts_Durations[Temp_stts_Durations.size() - 1].SampleDuration)
@@ -2658,6 +2662,8 @@ bool File_Mpeg4::BookMark_Needed()
                                     if (Temp->second.FirstUsedOffset==(int64u)-1)
                                         Temp->second.FirstUsedOffset=mdat_Pos_Temp2.Offset;
                                     mdat_Pos.push_back(mdat_Pos_Temp2);
+                                    if (Temp->second.IsCaption)
+                                        mdat_Pos_Caption.push_back(mdat_Pos_Temp2);
                                 }
                                 Chunk_Offset += Size;
                                 if (Chunk_Offset >= File_Size)
@@ -2667,7 +2673,7 @@ bool File_Mpeg4::BookMark_Needed()
                                 }
                                 Chunk_FrameCount++;
                                 }
-                            if (!Temp->second.TimeCode && Chunk_FrameCount >= FrameCount_MaxPerStream)
+                            if (!Temp->second.TimeCode && !Temp->second.IsCaption && Chunk_FrameCount >= FrameCount_MaxPerStream)
                                 break;
                         }
 

Source/MediaInfo/Multiple/File_Mpeg4.h
@@ -454,6 +454,19 @@ private :
     int16u                                  channelcount;
     int8u                                   Version_Temp; //Used when box version must be provided to nested boxes
 
+    struct mdat_Pos_Type
+    {
+        int64u Offset;
+        int64u Size;
+        int32u StreamID;
+        int32u Reserved1;
+        int64u Reserved2;
+        bool operator<(const mdat_Pos_Type& r) const
+        {
+            return Offset<r.Offset;
+        }
+    };
+
     //Data
     struct stream
     {
@@ -532,8 +545,10 @@ private :
         bool                    HasForcedSamples;
         bool                    AllForcedSamples;
         bool                    IsImage;
+        bool                    IsCaption;
         bool                    tkhd_Found;
         int32u                  TrackID;
+        std::vector<mdat_Pos_Type> mdat_Pos;
         std::vector<int32u>     CC;
         std::vector<int32u>     CCFor;
         std::vector<int32u>     FallBackTo;
@@ -628,6 +643,7 @@ private :
             HasForcedSamples=false;
             AllForcedSamples=false;
             IsImage=false;
+            IsCaption=false;
             tkhd_Found=false;
             CleanAperture_Width=0;
             CleanAperture_Height=0;
@@ -679,18 +695,6 @@ private :
     size_t* File_Buffer_Size_Hint_Pointer;
 
     //Positions
-    struct mdat_Pos_Type
-    {
-        int64u Offset;
-        int64u Size;
-        int32u StreamID;
-        int32u Reserved1;
-        int64u Reserved2;
-        bool operator<(const mdat_Pos_Type& r) const
-        {
-            return Offset<r.Offset;
-        }
-    };
     typedef std::vector<mdat_Pos_Type> mdat_pos;
     static bool mdat_pos_sort (const File_Mpeg4::mdat_Pos_Type &i,const File_Mpeg4::mdat_Pos_Type &j) { return (i.Offset<j.Offset); }
     void IsParsing_mdat_Set();
@@ -698,6 +702,7 @@ private :
     void TimeCodeTrack_Check(stream &Stream_Temp, size_t Pos, int32u StreamID);
     #endif //MEDIAINFO_DEMUX
     mdat_pos mdat_Pos;
+    mdat_pos mdat_Pos_Caption;
     mdat_Pos_Type* mdat_Pos_Temp;
     mdat_Pos_Type* mdat_Pos_Temp_ToJump;
     mdat_Pos_Type* mdat_Pos_Max;

Source/MediaInfo/Multiple/File_Mpeg4_Elements.cpp
@@ -2171,8 +2171,84 @@ void File_Mpeg4::mdat_xxxx()
                         if (mdat_Pos_Item->StreamID!=(int32u)Element_Code)
                             mdat_Pos_New.push_back(*mdat_Pos_Item);
                 }
-                mdat_Pos=std::move(mdat_Pos_New);
-                std::sort(mdat_Pos.begin(), mdat_Pos.end(), &mdat_pos_sort);
+                if (!mdat_Pos_New.empty())
+                {
+                    mdat_Pos=std::move(mdat_Pos_New);
+                    std::sort(mdat_Pos.begin(), mdat_Pos.end(), &mdat_pos_sort);
+                }
+                else
+                {
+                    mdat_Pos=move(mdat_Pos_Caption);
+                    std::sort(mdat_Pos.begin(), mdat_Pos.end(), &mdat_pos_sort);
+                    size_t mdat_Pos_Min=(size_t)-1;
+                    size_t mdat_Pos_Max=0;
+                    for (auto& Stream : Streams)
+                    {
+                        if (Stream.second.IsCaption)
+                        {
+                            Stream.second.Parsers.front()->Open_Buffer_Unsynch();
+                            int64u ProbeCaption_mdatPos=(int64u)-1;
+                            int64u ProbeCaption_mdatDur=(int64u)-1;
+                            if (Stream.second.stts_Duration && Stream.second.stts_FrameCount && Stream.second.mdhd_TimeScale)
+                            {
+                                auto FrameRateRatio=(float)Stream.second.stts_Duration/Stream.second.stts_FrameCount;
+                                auto FrameRate=Stream.second.mdhd_TimeScale/FrameRateRatio;
+                                auto Duration=(float)Stream.second.stts_Duration/Stream.second.mdhd_TimeScale;
+                                if (FrameRate)
+                                {
+                                    auto Probe=Config->File_ProbeCaption_Get(ParserName);
+                                    switch (Probe.Start_Type) {
+                                        case config_probe_dur:
+                                            ProbeCaption_mdatPos=Probe.Start*FrameRate;
+                                            break;
+                                        case config_probe_size:
+                                            Probe.Start=Probe.Start*100/File_Size; //File pos is not relevant there
+                                            if (!Probe.Start)
+                                                Probe.Start=50;
+                                            // Fall through
+                                        case config_probe_percent:
+                                            ProbeCaption_mdatPos=Stream.second.stts_FrameCount*Probe.Start/100;
+                                            break;
+                                    }
+                                    switch (Probe.Duration_Type) {
+                                        case config_probe_dur:
+                                            ProbeCaption_mdatDur=Probe.Duration*FrameRate;
+                                            break;
+                                        case config_probe_size:
+                                            Probe.Duration=Probe.Duration*100/File_Size; //File pos is not relevant there
+                                            if (!Probe.Duration)
+                                                Probe.Duration++;
+                                            // Fall through
+                                        case config_probe_percent:
+                                            ProbeCaption_mdatDur=Stream.second.stts_FrameCount*Probe.Duration/100;
+                                            break;
+                                    }
+                                }
+                            }
+                            if (ProbeCaption_mdatPos!=(int64u)-1 && ProbeCaption_mdatDur!=(int64u)-1)
+                            {
+                                size_t mdat_Pos_PerStream=0;
+                                auto ProbeCaption_mdatPosEnd=ProbeCaption_mdatPos+ProbeCaption_mdatDur;
+                                for (size_t i=0; i<mdat_Pos.size(); i++)
+                                {
+                                    const auto& mdat=mdat_Pos[i];
+                                    if (mdat.StreamID==Stream.first)
+                                    {
+                                        mdat_Pos_PerStream++;
+                                        if (mdat_Pos_PerStream==ProbeCaption_mdatPos && mdat_Pos_Min>i)
+                                            mdat_Pos_Min=i;
+                                        if (mdat_Pos_PerStream==ProbeCaption_mdatPosEnd && mdat_Pos_Max<=i)
+                                            mdat_Pos_Max=i+1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (mdat_Pos_Max && mdat_Pos_Max<mdat_Pos.size())
+                        mdat_Pos.resize(mdat_Pos_Max);
+                    if (mdat_Pos_Min<mdat_Pos.size())
+                        mdat_Pos.erase(mdat_Pos.begin(), mdat_Pos.begin()+mdat_Pos_Min);
+                }
                 if (mdat_Pos.empty())
                 {
                     mdat_Pos_Temp=nullptr;
@@ -3120,7 +3196,7 @@ void File_Mpeg4::moof_traf_trun()
             //Filling
             Stream->second.stsz_StreamSize+=sample_size;
             Stream->second.stsz_Total.push_back(sample_size);
-            if (Stream->second.stsz.size()<FrameCount_MaxPerStream || Stream->second.TimeCode)
+            if (Stream->second.stsz.size()<FrameCount_MaxPerStream || Stream->second.TimeCode || Streams[moov_trak_tkhd_TrackID].IsCaption)
                 Stream->second.stsz.push_back(sample_size);
             if (Stream->second.StreamKind==Stream_Text && sample_size>2)
                 Stream->second.stsz_MoreThan2_Count++;
@@ -4920,7 +4996,7 @@ void File_Mpeg4::moov_trak_mdia_minf_stbl_co64()
         return;
 
     std::vector<int64u> &stco=Streams[moov_trak_tkhd_TrackID].stco;
-    stco.resize((Count<FrameCount_MaxPerStream || Streams[moov_trak_tkhd_TrackID].TimeCode)?Count:FrameCount_MaxPerStream);
+    stco.resize((Count<FrameCount_MaxPerStream || Streams[moov_trak_tkhd_TrackID].TimeCode || Streams[moov_trak_tkhd_TrackID].IsCaption)?Count:FrameCount_MaxPerStream);
     int64u* stco_Data=&stco[0];
 
     for (int32u Pos=0; Pos<Count; Pos++)
@@ -4936,7 +5012,7 @@ void File_Mpeg4::moov_trak_mdia_minf_stbl_co64()
         Offset=BigEndian2int64u(Buffer+Buffer_Offset+(size_t)Element_Offset);
         Element_Offset+=8;
 
-        if (Pos<FrameCount_MaxPerStream || Streams[moov_trak_tkhd_TrackID].TimeCode)
+        if (Pos<FrameCount_MaxPerStream || Streams[moov_trak_tkhd_TrackID].TimeCode || Streams[moov_trak_tkhd_TrackID].IsCaption)
         {
             (*stco_Data)=Offset;
             stco_Data++;
@@ -5110,7 +5186,7 @@ void File_Mpeg4::moov_trak_mdia_minf_stbl_stco()
         Offset=BigEndian2int32u(Buffer+Buffer_Offset+(size_t)Element_Offset);
         Element_Offset+=4;
 
-        if (Pos<FrameCount_MaxPerStream || Streams[moov_trak_tkhd_TrackID].TimeCode)
+        if (Pos<FrameCount_MaxPerStream || Streams[moov_trak_tkhd_TrackID].TimeCode || Streams[moov_trak_tkhd_TrackID].IsCaption)
             Streams[moov_trak_tkhd_TrackID].stco.push_back(Offset);
     }
 }
@@ -5196,7 +5272,7 @@ void File_Mpeg4::moov_trak_mdia_minf_stbl_stsc()
         */
 
         //Faster
-        if (Pos<FrameCount_MaxPerStream || Streams[moov_trak_tkhd_TrackID].TimeCode)
+        if (Pos<FrameCount_MaxPerStream || Streams[moov_trak_tkhd_TrackID].TimeCode || Streams[moov_trak_tkhd_TrackID].IsCaption)
         {
             if (Element_Offset+12>Element_Size)
                 break; //Problem
@@ -6139,6 +6215,7 @@ void File_Mpeg4::moov_trak_mdia_minf_stbl_stsd_xxxxText()
             //Creating the parser
             File_Mpeg4* Parser=new File_Mpeg4;
             Streams[moov_trak_tkhd_TrackID].Parsers.push_back(Parser);
+            Streams[moov_trak_tkhd_TrackID].IsCaption=true;
         }
         #if defined(MEDIAINFO_CDP_YES)
         if (MediaInfoLib::Config.CodecID_Get(Stream_Text, InfoCodecID_Format_Mpeg4, CodecID, InfoCodecID_Format)==__T("EIA-708"))
@@ -6148,6 +6225,7 @@ void File_Mpeg4::moov_trak_mdia_minf_stbl_stsd_xxxxText()
             Parser->WithAppleHeader=true;
             Parser->AspectRatio=((float)16)/9; //TODO: this is hardcoded, must adapt it to the real video aspect ratio
             Streams[moov_trak_tkhd_TrackID].Parsers.push_back(Parser);
+            Streams[moov_trak_tkhd_TrackID].IsCaption=true;
         }
         #endif
         #if defined(MEDIAINFO_TTML_YES)
@@ -8609,7 +8687,7 @@ void File_Mpeg4::moov_trak_mdia_minf_stbl_stsz()
 
             Stream->second.stsz_StreamSize+=Size;
             Stream->second.stsz_Total.push_back(Size);
-            if (Pos<FrameCount_MaxPerStream || Stream->second.TimeCode)
+            if (Pos<FrameCount_MaxPerStream || Stream->second.TimeCode || Streams[moov_trak_tkhd_TrackID].IsCaption)
                 Stream->second.stsz.push_back(Size);
             if (IsTimedText && Size>2)
                 TimedText_Count++;

Source/MediaInfo/Text/File_Cdp.cpp
@@ -354,7 +354,8 @@ void File_Cdp::Data_Parse()
     }
 
     FILLING_BEGIN();
-        Frame_Count++;
+        if (Status[IsFilled] && Frame_Count>=1024 && Config->ParseSpeed<1.0)
+            Fill();
         if (!IsSub && Config->ParseSpeed<1.0 && Frame_Count>=300)
             Finish();
     FILLING_END();

commit 3f8e5517d75b95bec3089317c0dba5813174a30d

MXF: Fix OP47 Teletext not being probed as a Text track for short-duration files.
This fixes a regression from commit 6e03ccb27a293dc5. Prior to this commit,
MXF would do a supplemental probe of 64 MiB at the midpoint of the file to
identify captions. After this commit, the supplemental probe's start-point and
window-size were made configurable, defaulting to a start-point of 50% by byte-
offset and a window size of 30 seconds (extrapolated from average bitrate.)

However, for very short files, there may not _be_ 30 more seconds of content
after the midpoint of the file. This commit handles that scenario by clamping
the size of the probe window so it fits in the file, rather than skipping the
probe altogether.

Source/MediaInfo/Multiple/File_Mxf.cpp
@@ -16044,16 +16044,29 @@ bool File_Mxf::BookMark_Needed()
                 ProbeCaptionByteDur=ContentSize/100*Probe.Duration;
                 break;
         }
-        auto MaxOffset=ProbeCaptionBytePos+ProbeCaptionByteDur;
-        auto CurrentEnd=File_Offset+Buffer_Offset;
-        if (ProbeCaptionBytePos!=(int64u)-1 && ProbeCaptionByteDur!=(int64u)-1 && File_Size/2>ProbeCaptionByteDur)
+        if (ProbeCaptionBytePos!=(int64u)-1 && ProbeCaptionByteDur!=(int64u)-1)
         {
-            IsParsingMiddle_MaxOffset=MaxOffset;
-            GoTo(ProbeCaptionBytePos);
-            Open_Buffer_Unsynch();
-            IsParsingEnd=false;
-            IsCheckingRandomAccessTable=false;
-            Streams_Count=(size_t)-1;
+            if (File_Size/2<ProbeCaptionByteDur &&
+                HeaderSize+ContentSize-ProbeCaptionBytePos>0x4000000 && ContentSize/2>0x4000000)
+            {
+                // Clamp probe window to file size as long as it can be at least
+                // 64 MB. (For example, if a file is under 60 seconds total and
+                // is probed with the default duration window of 30 seconds,
+                // then there will be less than 30s of content after the halfway
+                // point of the file.)
+                ProbeCaptionByteDur=std::min(HeaderSize+ContentSize-ProbeCaptionBytePos, ContentSize/2);
+            }
+            auto MaxOffset=ProbeCaptionBytePos+ProbeCaptionByteDur;
+            auto CurrentEnd=File_Offset+Buffer_Offset;
+            if (File_Size/2>=ProbeCaptionByteDur)
+            {
+                IsParsingMiddle_MaxOffset=MaxOffset;
+                GoTo(ProbeCaptionBytePos);
+                Open_Buffer_Unsynch();
+                IsParsingEnd=false;
+                IsCheckingRandomAccessTable=false;
+                Streams_Count=(size_t)-1;
+            }
         }
     }
 