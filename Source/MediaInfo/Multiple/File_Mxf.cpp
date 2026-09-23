MediaArea/MediaInfoLib

commit 7fe72ac77089a36e23173c8854c49a5bf3408ca2

x MXF: quicker parsing when fast parsing is requested

Source/MediaInfo/Multiple/File_Mxf.cpp
@@ -4895,6 +4895,15 @@ void File_Mxf::Read_Buffer_AfterParsing()
         if (File_GoTo==(int64u)-1)
             GoToFromEnd(0);
     }
+
+    if (IsSub)
+    {
+        Frame_Count++;
+        if (Frame_Count_NotParsedIncluded!=(int64u)-1)
+            Frame_Count_NotParsedIncluded++;
+        if (!Status[IsFilled] && Config->ParseSpeed<=0)
+            Fill();
+    }
 }
 
 //---------------------------------------------------------------------------
@@ -5102,7 +5111,7 @@ void File_Mxf::Read_Buffer_Unsynched()
 #if (MEDIAINFO_DEMUX || MEDIAINFO_SEEK) && defined(MEDIAINFO_FILE_YES)
 bool File_Mxf::DetectDuration ()
 {
-    if (Duration_Detected)
+    if (Config->ParseSpeed<=0 || Duration_Detected)
         return false;
 
     MediaInfo_Internal MI;
@@ -6640,7 +6649,6 @@ void File_Mxf::Data_Parse()
                             Parsing_Size=Element_Size-Element_Offset; // There is a problem
                         if (Parsing_Size>Array_Size)
                             Parsing_Size=Array_Size; // There is a problem
-                        (*Parser)->Frame_Count=Frame_Count;
                         (*Parser)->Frame_Count_NotParsedIncluded=Frame_Count_NotParsedIncluded;
                         Open_Buffer_Continue((*Parser), Buffer+Buffer_Offset+(size_t)(Element_Offset), Parsing_Size);
                         if ((Code_Compare4&0xFF00FF00)==0x17000100 && LineNumber==21 && (*Parser)->Count_Get(Stream_Text)==0)
@@ -6655,8 +6663,18 @@ void File_Mxf::Data_Parse()
                             Skip_XX(Array_Size-Parsing_Size,    "Padding");
                         Element_End0();
                     }
-                    if (IsSub)
-                        Frame_Count++;
+                    if (!Essence->second.IsFilled && (!Count || (Essence->second.Parsers.size()==1 && Essence->second.Parsers[0]->Status[IsFilled])))
+                    {
+                        if (Streams_Count>0)
+                            Streams_Count--;
+                        Essence->second.IsFilled=true;
+                        if (Config->ParseSpeed<1.0 && IsSub)
+                        {
+                            Fill();
+                            Open_Buffer_Unsynch();
+                            Finish();
+                        }
+                    }
                 }
             }
             else
@@ -6836,7 +6854,7 @@ void File_Mxf::Data_Parse()
     }
 
     if ((!IsParsingEnd && IsParsingMiddle_MaxOffset==(int64u)-1 && Config->ParseSpeed<1.0)
-     && ((!IsSub && File_Offset>=Buffer_PaddingBytes+0x4000000) //TODO: 64 MB by default (security), should be changed
+     && ((!IsSub && File_Offset>=Buffer_PaddingBytes+(Config->ParseSpeed<=0?0x1000000:0x4000000)) //TODO: 64 MB by default (security), should be changed
       || (Streams_Count==0 && !Descriptors.empty())))
     {
         Fill();
@@ -16697,6 +16715,9 @@ void File_Mxf::Get_BER(int64u &Value, const char* Name)
 //---------------------------------------------------------------------------
 void File_Mxf::ChooseParser(const essences::iterator &Essence, const descriptors::iterator &Descriptor)
 {
+    if (Config->ParseSpeed<0)
+        return;
+
     if ((Descriptor->second.EssenceCompression.hi&0xFFFFFFFFFFFFFF00LL)!=0x060E2B3404010100LL || (Descriptor->second.EssenceCompression.lo&0xFF00000000000000LL)!=0x0400000000000000LL)
         return ChooseParser__FromEssenceContainer (Essence, Descriptor);
 
@@ -16948,6 +16969,9 @@ void File_Mxf::ChooseParser__FromEssenceContainer(const essences::iterator &Esse
 //---------------------------------------------------------------------------
 void File_Mxf::ChooseParser__FromEssence(const essences::iterator &Essence, const descriptors::iterator &Descriptor)
 {
+    if (Config->ParseSpeed<0)
+        return;
+
     int32u Code_Compare3=Code.lo>>32;
 
     switch (Code_Compare3)
@@ -18152,7 +18176,7 @@ bool File_Mxf::BookMark_Needed()
 {
     Frame_Count_NotParsedIncluded=(int64u)-1;
 
-    if (MayHaveCaptionsInStream && !IsSub && IsParsingEnd && File_Size!=(int64u)-1 && Config->ParseSpeed && Config->ParseSpeed<1 && IsParsingMiddle_MaxOffset==(int64u)-1 && File_Size/2>0x4000000) //TODO: 64 MB by default; // Do not search in the middle of the file if quick pass or full pass
+    if (MayHaveCaptionsInStream && !IsSub && IsParsingEnd && File_Size!=(int64u)-1 && Config->ParseSpeed>0 && Config->ParseSpeed<1 && IsParsingMiddle_MaxOffset==(int64u)-1 && File_Size/2>0x4000000) //TODO: 64 MB by default; // Do not search in the middle of the file if quick pass or full pass
     {
         IsParsingMiddle_MaxOffset=File_Size/2+0x4000000; //TODO: 64 MB by default;
         GoTo(File_Size/2);

Source/MediaInfo/Audio/File_Ac3.cpp
@@ -1811,7 +1811,7 @@ bool File_Ac3::Synchronize()
 void File_Ac3::Synched_Init()
 {
     if (!Frame_Count_Valid)
-        Frame_Count_Valid=Config->ParseSpeed>=0.3?32:2;
+        Frame_Count_Valid=Config->ParseSpeed>=0.3?32:(IsSub?1:2);
 
     //FrameInfo
     PTS_End=0;

Source/MediaInfo/Audio/File_Ac4.cpp
@@ -1866,7 +1866,7 @@ void File_Ac4::Synched_Init()
     Accept();
     
     if (!Frame_Count_Valid)
-        Frame_Count_Valid=Config->ParseSpeed>=0.3?128:2;
+        Frame_Count_Valid=Config->ParseSpeed>=0.3?128:(IsSub?1:2);
 
     //FrameInfo
     PTS_End=0;

Source/MediaInfo/Audio/File_Dts.cpp
@@ -766,7 +766,7 @@ bool File_Dts::FileHeader_Begin()
 
     //All should be OK...
     if (!Frame_Count_Valid)
-        Frame_Count_Valid=Config->ParseSpeed>=0.3?32:2;
+        Frame_Count_Valid=Config->ParseSpeed>=0.3?32:(IsSub?1:2);
     return true;
 }
 

Source/MediaInfo/Audio/File_Iab.cpp
@@ -133,7 +133,7 @@ File_Iab::File_Iab()
     StreamSource=IsStream;
 
     //In
-    Frame_Count_Valid=2;
+    Frame_Count_Valid=1;
 
     //Temp
     SampleRate=(int8u)-1;
@@ -153,15 +153,14 @@ File_Iab::~File_Iab()
 //---------------------------------------------------------------------------
 void File_Iab::Streams_Fill_ForAdm()
 {
-
-    if (Objects.empty())
+    if (Frame.Objects.empty())
         return;
     Fill(Stream_Audio, 0, "NumberOfProgrammes", 1);
     Fill(Stream_Audio, 0, "NumberOfContents", 1);
-    Fill(Stream_Audio, 0, "NumberOfObjects", Objects.size());
-    Fill(Stream_Audio, 0, "NumberOfPackFormats", Objects.size());
+    Fill(Stream_Audio, 0, "NumberOfObjects", Frame.Objects.size());
+    Fill(Stream_Audio, 0, "NumberOfPackFormats", Frame.Objects.size());
     size_t numberOfChannelFormats=0;
-    for (const auto& Object : Objects)
+    for (const auto& Object : Frame.Objects)
     {
         numberOfChannelFormats+=Object.ChannelLayout.empty()?1:Object.ChannelLayout.size();
     }
@@ -188,7 +187,7 @@ void File_Iab::Streams_Fill_ForAdm()
         Fill(Stream_Audio, 0, (string(O) + " Pos").c_str(), 0);
         Fill_SetOptions(Stream_Audio, 0, (string(O) + " Pos").c_str(), "N NIY");
         ZtringList SubstreamPos, SubstreamNum;
-        for (size_t i=0; i<Objects.size(); i++)
+        for (size_t i=0; i<Frame.Objects.size(); i++)
         {
             SubstreamPos.push_back(Ztring::ToZtring(i));
             SubstreamNum.push_back(Ztring::ToZtring(i+1));
@@ -204,9 +203,9 @@ void File_Iab::Streams_Fill_ForAdm()
     }
 
     numberOfChannelFormats=0;
-    for (size_t i=0; i<Objects.size(); i++)
+    for (size_t i=0; i<Frame.Objects.size(); i++)
     {
-        const auto& Object=Objects[i];
+        const auto& Object=Frame.Objects[i];
         auto O="Object"+to_string(i);
         Fill(Stream_Audio, 0, O.c_str(), "Yes");
         Fill(Stream_Audio, 0, (O + " Pos").c_str(), i);
@@ -242,9 +241,9 @@ void File_Iab::Streams_Fill_ForAdm()
     }
 
     numberOfChannelFormats = 0;
-    for (size_t i=0; i<Objects.size(); i++)
+    for (size_t i=0; i<Frame.Objects.size(); i++)
     {
-        const auto& Object=Objects[i];
+        const auto& Object=Frame.Objects[i];
         auto O="PackFormat"+to_string(i);
         Fill(Stream_Audio, 0, O.c_str(), "Yes");
         Fill(Stream_Audio, 0, (O + " Pos").c_str(), i);
@@ -279,9 +278,9 @@ void File_Iab::Streams_Fill_ForAdm()
     }
 
     numberOfChannelFormats=0;
-    for (size_t i=0; i<Objects.size(); i++)
+    for (size_t i=0; i<Frame.Objects.size(); i++)
     {
-        const auto& Object=Objects[i];
+        const auto& Object=Frame.Objects[i];
         auto O_Base="ChannelFormat";
         if (Object.ChannelLayout.empty())
         {
@@ -308,9 +307,9 @@ void File_Iab::Streams_Fill_ForAdm()
     }
 
     numberOfChannelFormats=0;
-    for (size_t i=0; i<Objects.size(); i++)
+    for (size_t i=0; i<Frame.Objects.size(); i++)
     {
-        const auto& Object=Objects[i];
+        const auto& Object=Frame.Objects[i];
         auto O_Base="TrackUID";
         if (Object.ChannelLayout.empty())
         {
@@ -362,9 +361,9 @@ void File_Iab::Streams_Fill_ForAdm()
     }
 
     numberOfChannelFormats=0;
-    for (size_t i=0; i<Objects.size(); i++)
+    for (size_t i=0; i<Frame.Objects.size(); i++)
     {
-        const auto& Object=Objects[i];
+        const auto& Object=Frame.Objects[i];
         auto O_Base="TrackFormat";
         if (Object.ChannelLayout.empty())
         {
@@ -400,9 +399,9 @@ void File_Iab::Streams_Fill_ForAdm()
     }
 
     numberOfChannelFormats=0;
-    for (size_t i=0; i<Objects.size(); i++)
+    for (size_t i=0; i<Frame.Objects.size(); i++)
     {
-        const auto& Object=Objects[i];
+        const auto& Object=Frame.Objects[i];
         auto O_Base="StreamFormat";
         if (Object.ChannelLayout.empty())
         {
@@ -535,6 +534,14 @@ void File_Iab::Data_Parse()
         case 0x00000400: Element_Name("Audio Data PCM"); AudioDataPCM(); break;
         default: Element_Name(Ztring().From_CC4((int32u)Element_Code)); Skip_XX(Element_Size, "Data");
     }
+
+    if ((Element_Code!=0x00000008 || Element_Offset==Element_Size) && Element_Size>=Element_TotalSize_Get(Element_Level-1))
+    {
+        Frame.Objects=move(F.Objects);
+        Frame_Count++;
+        if (!Status[IsFilled] && Frame_Count>=Frame_Count_Valid)
+            Finish();
+    }
 }
 
 //---------------------------------------------------------------------------
@@ -553,23 +560,16 @@ void File_Iab::IAFrame()
         Get_Plex8 (MaxRendered,                                     "MaxRendered");
         Get_Plex8 (SubElementCount,                                 "SubElementCount");
         Element_ThisIsAList();
-        if (Frame_Count+1<Frame_Count_Valid)
-            Objects.clear();
+        Frame.Objects=move(F.Objects);
     }
     else
         Skip_XX(Element_Size-Element_Offset,                        "Unknown");
-
-    FILLING_BEGIN();
-        Frame_Count++;
-        if (!Status[IsFilled] && Frame_Count>=Frame_Count_Valid)
-            Finish();
-    FILLING_END();
 }
 
 //---------------------------------------------------------------------------
 void File_Iab::BedDefinition()
 {
-    Objects.resize(Objects.size()+1);
+    F.Objects.resize(F.Objects.size()+1);
 
     //Parsing
     int32u ChannelCount;
@@ -604,7 +604,7 @@ void File_Iab::BedDefinition()
                 Skip_S1(10,                                     "ChannelDecorCoef");
         }
         Element_End0();
-        Objects.back().ChannelLayout.push_back(ChannelID);
+        F.Objects.back().ChannelLayout.push_back(ChannelID);
     }
     Skip_S2(10,                                                 "0x180");
     if (Data_BS_Remain()%8)
@@ -625,7 +625,7 @@ void File_Iab::BedDefinition()
 //---------------------------------------------------------------------------
 void File_Iab::ObjectDefinition()
 {
-    Objects.resize(Objects.size()+1);
+    F.Objects.resize(F.Objects.size()+1);
 
     //Parsing
     int8u AudioDescription;

Source/MediaInfo/Audio/File_Iab.h
@@ -57,7 +57,12 @@ private :
     {
         vector<int32u> ChannelLayout;
     };
-    vector<object> Objects;
+    struct frame
+    {
+        vector<object> Objects;
+    };
+    frame                       Frame;
+    frame                       F;
 
     //Helpers
     void Get_Plex(int8u Bits, int32u& Info, const char* Name);

Source/MediaInfo/Audio/File_Mpega.cpp
@@ -584,7 +584,7 @@ bool File_Mpega::FileHeader_Begin()
 
     //Seems OK
     if (!Frame_Count_Valid)
-        Frame_Count_Valid=Config->ParseSpeed>=0.5?128:(Config->ParseSpeed>=0.3?32:4);
+        Frame_Count_Valid=Config->ParseSpeed>=0.5?128:(Config->ParseSpeed>=0.3?32:(IsSub?1:4));
     return true;
 }
 

Source/MediaInfo/Audio/File_SmpteSt0331.cpp
@@ -160,6 +160,7 @@ void File_SmpteSt0331::Read_Buffer_Continue()
 
     Skip_XX(Element_Size-4,                             "Data");
 
+    Frame_Count++;
     Frame_Count_InThisBlock++;
     if (Frame_Count_NotParsedIncluded!=(int64u)-1)
         Frame_Count_NotParsedIncluded++;
@@ -179,22 +180,10 @@ void File_SmpteSt0331::Read_Buffer_Continue()
     #endif //MEDIAINFO_DEMUX
 
     FILLING_BEGIN();
-    if (!Status[IsAccepted])
-    {
-        Accept("SMPTE ST 331");
-
-        int8u Channels=0;
-        for (int8u Pos=0; Pos<8; Pos++)
-        {
-            if (Channels_valid&(1<<Pos))
-                Channels++;
-            Element_Offset+=4;
-        }
-
-        Stream_Prepare(Stream_Audio);
-        Fill(Stream_Audio, 0, Audio_Format, "PCM");
-        Fill(Stream_Audio, 0, Audio_Channel_s_, Channels);
-    }
+        if (!Status[IsAccepted])
+            Accept("SMPTE ST 331");
+        if (!Status[IsFilled])
+            Fill("SMPTE ST 331");
     FILLING_END();
 }
 

commit 2ccb5519be7ec1f8b09ca4926902d2d28e7db2bf

MXF: Fix premature Fill of ANC essence causing Teletext loss when content is sparse
Fixes a regression from commit 7fe72ac, which added an early-exit codepath for
teletext (or other ancillary data) in MXF files. However, this early-exit
activated prematurely for a 40s file where there was no visible caption content
until after 20s of duration. Other sparse ancillary-in-MXF files may hit this
issue as well.

This commit fixes the issue by applying stricter criteria to the early-exit
condition; see code comments for details.

Source/MediaInfo/Multiple/File_Mxf.cpp
@@ -6613,7 +6613,11 @@ void File_Mxf::Data_Parse()
                             Skip_XX(Array_Size-Parsing_Size,    "Padding");
                         Element_End0();
                     }
-                    if (!Essence->second.IsFilled && (!Count || (Essence->second.Parsers.size()==1 && Essence->second.Parsers[0]->Status[IsFilled])))
+                    if (!Essence->second.IsFilled && Essence->second.Parsers.size()==1
+                     // Either the sub-parser reports it is genuinely done (Status[IsFilled]),
+                     // or it never saw any ANC/VBI content at all (!Count && !IsAccepted)
+                     && ((!Count && !Essence->second.Parsers[0]->Status[IsAccepted])
+                         || Essence->second.Parsers[0]->Status[IsFilled]))
                     {
                         if (Streams_Count>0)
                             Streams_Count--;